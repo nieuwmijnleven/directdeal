8e1c2c945f3d36e622b4f2a2663db89232d48ec7
Jeon,Cheol <real-overcomer-jc@gmail.com>
2021-06-24 18:04:17 +0900

    added EFK stack kubernetes settings in order to collect and analysis logs

--- kubectl-apply.sh
+++ kubectl-apply.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+minikube ssh sudo ip link set docker0 promisc on
+
 KUBERNETES_CONFIG_PATH="./deployment/kubernetes"
 
+kubectl delete -f ${KUBERNETES_CONFIG_PATH}/log-elasticsearch.yml
+kubectl delete -f ${KUBERNETES_CONFIG_PATH}/log-kibana.yml
+kubectl delete -f ${KUBERNETES_CONFIG_PATH}/log-fluentd.yml
 kubectl delete -f ${KUBERNETES_CONFIG_PATH}/zookeeper-service.yml
 kubectl delete -f ${KUBERNETES_CONFIG_PATH}/mysql-service.yml
 kubectl delete -f ${KUBERNETES_CONFIG_PATH}/mongo-service.yml
@@ -14,15 +19,18 @@ kubectl delete -f ${KUBERNETES_CONFIG_PATH}/direct-deal-sale-catalog-service.yml
 kubectl delete -f ${KUBERNETES_CONFIG_PATH}/direct-deal-transaction-history-service.yml
 kubectl delete -f ${KUBERNETES_CONFIG_PATH}/direct-deal-gateway-service.yml
 
-kubectl apply -f ${KUBERNETES_CONFIG_PATH}/zookeeper-service.yml
-kubectl apply -f ${KUBERNETES_CONFIG_PATH}/mysql-service.yml
-kubectl apply -f ${KUBERNETES_CONFIG_PATH}/mongo-service.yml
-kubectl apply -f ${KUBERNETES_CONFIG_PATH}/redis-service.yml
-kubectl apply -f ${KUBERNETES_CONFIG_PATH}/kafka-service.yml
-kubectl apply -f ${KUBERNETES_CONFIG_PATH}/direct-deal-account-service.yml
-kubectl apply -f ${KUBERNETES_CONFIG_PATH}/direct-deal-chatting-service.yml
-kubectl apply -f ${KUBERNETES_CONFIG_PATH}/direct-deal-sale-service.yml
-kubectl apply -f ${KUBERNETES_CONFIG_PATH}/direct-deal-sale-catalog-service.yml
-kubectl apply -f ${KUBERNETES_CONFIG_PATH}/direct-deal-transaction-history-service.yml
-kubectl apply -f ${KUBERNETES_CONFIG_PATH}/direct-deal-gateway-service.yml
+kubectl create -f ${KUBERNETES_CONFIG_PATH}/log-elasticsearch.yml
+kubectl create -f ${KUBERNETES_CONFIG_PATH}/log-kibana.yml
+kubectl create -f ${KUBERNETES_CONFIG_PATH}/log-fluentd.yml
+kubectl create -f ${KUBERNETES_CONFIG_PATH}/zookeeper-service.yml
+kubectl create -f ${KUBERNETES_CONFIG_PATH}/mysql-service.yml
+kubectl create -f ${KUBERNETES_CONFIG_PATH}/mongo-service.yml
+kubectl create -f ${KUBERNETES_CONFIG_PATH}/redis-service.yml
+kubectl create -f ${KUBERNETES_CONFIG_PATH}/kafka-service.yml
+kubectl create -f ${KUBERNETES_CONFIG_PATH}/direct-deal-account-service.yml
+kubectl create -f ${KUBERNETES_CONFIG_PATH}/direct-deal-chatting-service.yml
+kubectl create -f ${KUBERNETES_CONFIG_PATH}/direct-deal-sale-service.yml
+kubectl create -f ${KUBERNETES_CONFIG_PATH}/direct-deal-sale-catalog-service.yml
+kubectl create -f ${KUBERNETES_CONFIG_PATH}/direct-deal-transaction-history-service.yml
+kubectl create -f ${KUBERNETES_CONFIG_PATH}/direct-deal-gateway-service.yml
 
